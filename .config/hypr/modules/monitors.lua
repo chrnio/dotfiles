89e16b4317f24a5fc62dee626113f45e9d2a994a
Charan Vadapalli <sree.saicharan.vadapalli@gmail.com>
2026-06-23 03:03:25 +0530

    fix: remove unused window manager configurations

--- .config/hypr/modules/monitors.lua
+++
@@ -1,9 +0,0 @@
--- modules/monitors.lua
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
-
-hl.monitor({
-	output   = "",
-	mode     = "preferred",
-	position = "auto",
-	scale    = "auto",
-})
