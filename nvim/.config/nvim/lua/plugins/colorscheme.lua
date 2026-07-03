3c807115fe47bbd188b2d6b92e151d0c0784267c
Charan Vadapalli <sree.saicharan.vadapalli@gmail.com>
2026-07-04 00:59:59 +0530

    feat: add tree-sitter configuration

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,6 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "night",
-
       transparent = true,
 
       styles = {
@@ -15,14 +14,42 @@ return {
         floats = "transparent",
       },
 
-      on_highlights = function(hl, c)
-        -- remove italics everywhere
+      on_highlights = function(hl, _)
+        -- Remove italics everywhere
         for _, group in pairs(hl) do
           if type(group) == "table" then
             group.italic = false
           end
         end
       end,
+    },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      no_italic = true,
+    },
+  },
+
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_transparent_background = 2
+      vim.g.gruvbox_material_enable_italic = false
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
     },
   },
 }
