af1033dc44fa6c28a2799407258a06a98dc83f7c
Charan Vadapalli <sree.saicharan.vadapalli@gmail.com>
2026-08-24 22:47:31 +0530

    feat: add new nvim configuration

---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+      transparent = false,
+
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = false },
+        functions = {},
+        variables = {},
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+
+      on_highlights = function(hl, _)
+        -- Remove italics everywhere
+        for _, group in pairs(hl) do
+          if type(group) == "table" then
+            group.italic = false
+          end
+        end
+      end,
+    },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = false,
+      no_italic = true,
+    },
+  },
+
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    init = function()
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_transparent_background = 2
+      vim.g.gruvbox_material_enable_italic = false
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
